3455b1a66fe3d58114fe01e4c9a6f8759c2c4bce
Nguyễn Hải <yingnhanviengg@yh5fg.onmicrosoft.com>
2023-02-26 00:46:15 +0700

    .

--- ATM_CONSOLE_APPLICATION/Language/Language.cs
+++ ATM_CONSOLE_APPLICATION/Language/Language.cs
@@ -6,36 +6,29 @@ namespace ATM_CONSOLE_APPLICATION.Language
     {
         public static void Change_Language()
         {
+            Console.Clear();
             string[] Menu_Customer = { "English", "Vietnamese" };
-            while (true)
+            Common.UI();
+            AbstractLanguage language;
+            var menuSelection = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("")
+                .PageSize(10)
+                .AddChoices(Menu_Customer));
+            int selectedIndex = Array.IndexOf(Menu_Customer, menuSelection);
+            switch (selectedIndex)
             {
-                Common.UI();
-                AbstractLanguage language;
-                var menuSelection = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("")
-                    .PageSize(10)
-                    .AddChoices(Menu_Customer));
-
-                int selectedIndex = Array.IndexOf(Menu_Customer, menuSelection);
-                switch (selectedIndex)
-                {
-                    case 0:
-                        language = new English();
-                        language.ChangeLanguage();
-                        break;
-                    case 1:
-                        language = new VietNamese();
-                        language.ChangeLanguage();
-                        break;
-                    default:
-                        Common.PrintMessage_Console("Error", false);
-                        break;
-                }
-                if (selectedIndex == 0 || selectedIndex == 1)
-                {
+                case 0:
+                    language = new English();
+                    language.ChangeLanguage();
                     break;
-                }
+                case 1:
+                    language = new VietNamese();
+                    language.ChangeLanguage();
+                    break;
+                default:
+                    Common.PrintMessage_Console("Error", false);
+                    break;
             }
         }
     }
